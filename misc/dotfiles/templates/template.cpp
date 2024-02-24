2b29e812468ae2f33a4d37e2e280b7080f11ee86
Dmitry Ilvokhin <d@ilvokhin.com>
2024-02-24 18:36:32 +0000

    Add simple dotfiles management playbook

---
+++ misc/dotfiles/templates/template.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <utility>
+#include <string>
+#include <vector>
+#include <limits>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <set>
+
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+
+    return 0;
+}
